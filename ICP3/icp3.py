11c3469fafe1e1566f997caa007d6c350d65c079
ajwkc <andrewwilson88@gmail.com>
2020-02-19 12:23:24 -0600

    Completed ICP3

--- ICP3/icp3.py
+++ ICP3/icp3.py
@@ -1,4 +1,5 @@
 import json
+from nltk.corpus import wordnet as wn
 from stanfordcorenlp import StanfordCoreNLP
 
 
@@ -7,26 +8,65 @@ def main():
     nlp = StanfordCoreNLP('http://localhost', port=9000, timeout=30000)
 
     # Define a sentence for processing
-    sentence = 'Candace likes to eat vanilla ice cream with chocolate chips'
+    sentence = 'Bill likes to eat vanilla ice cream with chocolate chips.'
 
     # Define the annotation tags
     output = nlp.annotate(sentence, properties={
-        'annotators': 'tokenize, ssplit,pos,depparse,natlog,openie',
+        'annotators': 'tokenize, ssplit, pos, depparse, natlog, openie',
         'outputFormat': 'json',
         'openie.triple.strict': 'true',
-        'openie.max_entailments_per_clause':'1'
+        'openie.max_entailments_per_clause': '1'
     })
 
     # Print the JSON info in readable format
     a = json.loads(output)
     print('The subject, object and predicate of the given sentence are:\n')
-    print(a['sentences'][0]['openie'],'\n')
+    print(a['sentences'][0]['openie'], '\n')
     result = [a['sentences'][0]['openie'] for item in a]
     for i in result:
         for relation in i:
-            relationSent = relation['relation'],relation['subject'],relation['object']
+            relationSent = relation['relation'], relation['subject'], relation['object']
             print('The triplet of the given sentence is:\n')
-            print(relationSent)
+            print(relationSent, "\n\n")
+
+    # WordNet functionality
+    word = wn.synsets("worm")
+
+    # Print the word
+    print('The word is:' ,word[0].lemmas()[0].name(), '\n')
+
+    # Definition of the word:
+    print('The definition is: ', word[0].definition(), '\n')
+
+    # Examples of the word in use in sentences:
+    print('Examples of the word:', word[0].examples(), '\n')
+
+    # synonyms and antonyms using wordnet using word
+    synonyms = []
+    antonyms = []
+
+    for syn in wn.synsets("good"):
+        for l in syn.lemmas():
+            synonyms.append(l.name())
+            if l.antonyms():
+                antonyms.append(l.antonyms()[0].name())
+    print('The synonyms of good are: ')
+    print(set(synonyms))
+    print('\n')
+    print('The antonyms of good are: ')
+    print(set(antonyms))
+    print('\n')
+
+
+    print('Set of hyponyms:\n', word[0].hyponyms(), '\n')
+    print('Set of hypernyms:\n', word[0].hypernyms(), '\n')
+    print('Set of part-meronyms:\n', word[0].part_meronyms(), '\n')
+    print('Set of substance-meronyms:\n', word[0].substance_meronyms(), '\n')
+    print('Set of member-holonyms:\n', word[0].member_holonyms(), '\n')
+    print('Set of part-meronyms:\n', word[0].part_meronyms(), '\n')
+
+    print('Entailments of the word Breathe:\n', wn.synset('breathe.v.01').entailments())
+
 
 if __name__ == '__main__':
     main()
--- ICP4/icp4.py
+++ ICP4/icp4.py
@@ -3,20 +3,18 @@ from pyspark.sql import SQLContext
 from pyspark import SparkFiles
 
 
-sc = pyspark.SparkContext()
-sqlContext = SQLContext(sc)
+def main():
+    # Alias for SparkContext
+    sc = pyspark.SparkContext()
 
-df = sqlContext.read.csv(SparkFiles.get("/home/andrew/CS5560-KDM/ICP4/data.csv"), header=True, inferSchema= True)
-df.printSchema()
-#df.show(5, truncate = False)
+    # Iterate through the file and count each word
+    file = sc.textFile('/home/andrew/CS5560-KDM/ICP4/input.txt')
+    counts = file.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-#If you didn't set inderShema to True, here is what is happening to the type. There are all in string.
-#df_string = sqlContext.read.csv(SparkFiles.get("C:/Users/shs6g/Desktop/KDM/code/ICP4_V2/data.csv"), header=True, inferSchema=  False)
-#df_string.printSchema()
+    # Print the word count results
+    for x in counts.collect():
+        print(x)
 
 
-#You can select and show the rows with select and the names of the features. Below, gender and churn are selected.
-#df.select('gender','churn').show(5)
-
-#To get a summary statistics, of the data, you can use describe(). It will compute the :count, mean, standarddeviation, min, max
-#df.describe().show()
+if __name__ == '__main__':
+    main()
